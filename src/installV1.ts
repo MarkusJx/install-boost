a3acf5c6d261fec468dec9e8da2defc5cd1ff488
Markus <28785953+MarkusJx@users.noreply.github.com>
2022-09-02 15:39:29 +0200

    Add support for cache

--- src/installV1.ts
+++ src/installV1.ts
@@ -1,16 +1,43 @@
-import * as core from "@actions/core";
-import * as path from "path";
-import { cleanup, createDirectory, downloadBoost, getVersions, parseArguments, untarBoost } from "./shared";
+import * as core from '@actions/core';
+import * as path from 'path';
+import { restoreCache, saveCache } from './cache';
+import {
+    cleanup,
+    createDirectory,
+    downloadBoost,
+    getVersions,
+    Options,
+    parseArguments,
+    untarBoost,
+} from './shared';
 
-const VERSION_MANIFEST_ADDR: string = "https://raw.githubusercontent.com/actions/boost-versions/main/versions-manifest.json";
+const VERSION_MANIFEST_ADDR: string =
+    'https://raw.githubusercontent.com/actions/boost-versions/main/versions-manifest.json';
 
-export default async function installV1(boost_version: string, toolset: string, platform_version: string, BOOST_ROOT_DIR: string): Promise<void> {
-    console.log("Using legacy install method");
-    console.log("Downloading versions-manifest.json...");
+export default async function installV1(opts: Options): Promise<void> {
+    console.log('Using legacy install method');
+    if (opts.cache) {
+        console.log('Trying to retrieve cache...');
+        if (await restoreCache(opts)) {
+            console.log('Cache successfully restored');
+            return;
+        } else {
+            console.log('Cache miss');
+        }
+    }
+
+    console.log('Downloading versions-manifest.json...');
     const versions: Object[] = await getVersions(VERSION_MANIFEST_ADDR);
 
-    console.log("Parsing versions-manifest.json...");
-    const ver_data = parseArguments(versions, boost_version, toolset, platform_version);
+    const { boost_version, toolset, platform_version, BOOST_ROOT_DIR } = opts;
+
+    console.log('Parsing versions-manifest.json...');
+    const ver_data = parseArguments(
+        versions,
+        boost_version,
+        toolset,
+        platform_version
+    );
     const download_url = ver_data.url;
     const filename = ver_data.filename;
 
@@ -18,12 +45,12 @@ export default async function installV1(boost_version: string, toolset: string, 
     createDirectory(BOOST_ROOT_DIR);
     core.endGroup();
 
-    core.startGroup("Download Boost");
+    core.startGroup('Download Boost');
     await downloadBoost(download_url, path.join(BOOST_ROOT_DIR, filename));
     core.endGroup();
 
-    let base_dir: string = filename.substring(0, filename.lastIndexOf("."));
-    base_dir = base_dir.substring(0, base_dir.lastIndexOf("."));
+    let base_dir: string = filename.substring(0, filename.lastIndexOf('.'));
+    base_dir = base_dir.substring(0, base_dir.lastIndexOf('.'));
     const BOOST_ROOT: String = path.join(BOOST_ROOT_DIR, base_dir);
 
     core.debug(`Boost base directory: ${base_dir}`);
@@ -32,15 +59,20 @@ export default async function installV1(boost_version: string, toolset: string, 
     await untarBoost(base_dir, BOOST_ROOT_DIR);
     core.endGroup();
 
-    core.startGroup("Clean up");
+    core.startGroup('Clean up');
     cleanup(BOOST_ROOT_DIR, base_dir);
     core.endGroup();
 
-    core.startGroup("Set output variables");
+    core.startGroup('Set output variables');
     console.log(`Setting BOOST_ROOT to '${BOOST_ROOT}'`);
     console.log(`Setting BOOST_VER to '${base_dir}'`);
     core.endGroup();
 
-    core.setOutput("BOOST_ROOT", BOOST_ROOT);
-    core.setOutput("BOOST_VER", base_dir);
+    core.setOutput('BOOST_ROOT', BOOST_ROOT);
+    core.setOutput('BOOST_VER', base_dir);
+
+    if (opts.cache) {
+        console.log('Saving cache');
+        saveCache(opts);
+    }
 }
--- src/shared.ts
+++ src/shared.ts
@@ -1,28 +1,41 @@
-import * as core from "@actions/core";
-import * as request from "request";
-import * as fs from "fs";
-import * as progress from "request-progress";
-import * as path from "path";
-import { spawn } from "child_process";
+import * as core from '@actions/core';
+import * as request from 'request';
+import * as fs from 'fs';
+import * as progress from 'request-progress';
+import * as path from 'path';
+import { spawn } from 'child_process';
+
+export interface Options {
+    boost_version: string;
+    toolset: string;
+    platform_version: string;
+    BOOST_ROOT_DIR: string;
+    cache: boolean;
+}
+
+export interface OptionsV2 extends Options {
+    link: string;
+    arch: string;
+}
 
 export function getVersions(manifestAddress: string): Promise<object[]> {
     return new Promise((resolve, reject) => {
         const req = request.get(manifestAddress);
 
         // Append data to the data object
-        let dt = "";
-        req.on('data', data => {
+        let dt = '';
+        req.on('data', (data) => {
             dt += data;
         });
 
         // Resolve on end with the data
         req.on('end', () => {
-            core.debug("Downloaded data: " + dt);
+            core.debug('Downloaded data: ' + dt);
             resolve(JSON.parse(dt));
         });
 
         // Reject on error
-        req.on('error', err => {
+        req.on('error', (err) => {
             reject(err.message);
         });
     });
@@ -30,87 +43,135 @@ export function getVersions(manifestAddress: string): Promise<object[]> {
 
 /**
  * Create a directory
- * 
+ *
  * @param dir the directory to create
  */
 export function createDirectory(dir: string): void {
     if (!fs.existsSync(dir)) {
         console.log(`${dir} does not exist, creating it`);
         fs.mkdirSync(dir, { recursive: true });
-        console.log("Done.");
+        console.log('Done.');
     } else {
         console.log(`${dir} already exists, doing nothing`);
     }
 }
 
 type parsedVersion = {
-    url: string,
-    filename: string
-}
+    url: string;
+    filename: string;
+};
 
 /**
  * Try to find the specified boost version
- * 
+ *
  * @param versions the version object array from getVersions()
  * @param boost_version the requested boost version
  * @param toolset the requested toolset
  * @param platform_version the requested platform version
  * @returns the url and file name or throws an error if the requested version could not be found
  */
-export function parseArguments(versions: object[], boost_version: string, toolset: string | null, platform_version: string, link: string | null = null, arch: string | null = null): parsedVersion {
+export function parseArguments(
+    versions: object[],
+    boost_version: string,
+    toolset: string | null,
+    platform_version: string,
+    link: string | null = null,
+    arch: string | null = null
+): parsedVersion {
     let platform: string = process.platform;
-    if (platform === "darwin") {
-        platform = "macos";
-    } else if (platform === "win32") {
-        platform = "windows";
+    if (platform === 'darwin') {
+        platform = 'macos';
+    } else if (platform === 'win32') {
+        platform = 'windows';
     }
 
     for (let i = 0; i < versions.length; i++) {
         let cur: object = versions[i];
-        if (cur.hasOwnProperty("version") && cur["version"] == boost_version) {
-            let files: object[] = cur["files"];
+        if (cur.hasOwnProperty('version') && cur['version'] == boost_version) {
+            let files: object[] = cur['files'];
             for (let j = 0; j < files.length; j++) {
                 let file: object = files[j];
 
-                core.debug(`file platform: ${file["platform"]}`);
-                if (!file.hasOwnProperty("platform") || file["platform"] != platform) {
+                core.debug(`file platform: ${file['platform']}`);
+                if (
+                    !file.hasOwnProperty('platform') ||
+                    file['platform'] != platform
+                ) {
                     core.debug("File does not match param 'platform'");
                     continue;
                 }
 
-                core.debug(`file toolset: ${file["toolset"]}`);
-                if (toolset && (!file.hasOwnProperty("toolset") || file["toolset"] != toolset)) {
+                core.debug(`file toolset: ${file['toolset']}`);
+                if (
+                    toolset &&
+                    (!file.hasOwnProperty('toolset') ||
+                        file['toolset'] != toolset)
+                ) {
                     core.debug("File does not match param 'toolset'");
                     continue;
                 }
 
-                core.debug(`file platform version: ${file["platform_version"]}`);
-                if (platform_version.length > 0 && (!file.hasOwnProperty("platform_version") || file["platform_version"] != platform_version)) {
+                core.debug(
+                    `file platform version: ${file['platform_version']}`
+                );
+                if (
+                    platform_version.length > 0 &&
+                    (!file.hasOwnProperty('platform_version') ||
+                        file['platform_version'] != platform_version)
+                ) {
                     core.debug("File does not match param 'platform_version");
                     continue;
                 }
 
-                if (link && !file["link"]) {
-                    core.warning("The parameter 'link' was specified, which doesn't have any effect on this boost version");
-                } else if (link && file.hasOwnProperty("link") && link !== file["link"] && file["link"] !== "static+shared") {
+                if (link && !file['link']) {
+                    core.warning(
+                        "The parameter 'link' was specified, which doesn't have any effect on this boost version"
+                    );
+                } else if (
+                    link &&
+                    file.hasOwnProperty('link') &&
+                    link !== file['link'] &&
+                    file['link'] !== 'static+shared'
+                ) {
                     core.debug("File does not match param 'link'");
                     continue;
-                } else if (!link && file.hasOwnProperty("link") && file["link"] === "shared") {
-                    core.debug("The file's 'link' was set to 'shared', but 'link' was not specified, ignoring this file");
+                } else if (
+                    !link &&
+                    file.hasOwnProperty('link') &&
+                    file['link'] === 'shared'
+                ) {
+                    core.debug(
+                        "The file's 'link' was set to 'shared', but 'link' was not specified, ignoring this file"
+                    );
                     continue;
                 }
 
-                if (arch && !file["arch"]) {
-                    core.warning("The parameter 'arch' was specified, which doesn't have any effect on this boost version");
-                } else if (arch && file.hasOwnProperty("arch") && arch !== file["arch"]) {
+                if (arch && !file['arch']) {
+                    core.warning(
+                        "The parameter 'arch' was specified, which doesn't have any effect on this boost version"
+                    );
+                } else if (
+                    arch &&
+                    file.hasOwnProperty('arch') &&
+                    arch !== file['arch']
+                ) {
                     core.debug("File does not match param 'arch'");
                     continue;
-                } else if (!arch && file.hasOwnProperty("arch") && file["arch"] !== "x86") {
-                    core.debug("The file's 'arch' was not set to 'x86', but 'arch' was not specified, ignoring this file");
+                } else if (
+                    !arch &&
+                    file.hasOwnProperty('arch') &&
+                    file['arch'] !== 'x86'
+                ) {
+                    core.debug(
+                        "The file's 'arch' was not set to 'x86', but 'arch' was not specified, ignoring this file"
+                    );
                     continue;
                 }
 
-                return { url: file["download_url"], filename: file["filename"] };
+                return {
+                    url: file['download_url'],
+                    filename: file['filename'],
+                };
             }
 
             break;
@@ -122,7 +183,7 @@ export function parseArguments(versions: object[], boost_version: string, toolse
 
 /**
  * Download boost
- * 
+ *
  * @param url the url to download from
  * @param outFile the file to download to
  */
@@ -130,9 +191,9 @@ export function downloadBoost(url: string, outFile: string): Promise<void> {
     return new Promise((resolve, reject) => {
         // Get the request with progress
         const req = progress(request(url));
-        req.on('progress', state => {
+        req.on('progress', (state) => {
             // Log the progress
-            core.debug(`Progress state: ${JSON.stringify(state)}`)
+            core.debug(`Progress state: ${JSON.stringify(state)}`);
             const percent: number = state.percent * 100;
             console.log(`Download progress: ${percent.toFixed(2)}%`);
         });
@@ -142,12 +203,12 @@ export function downloadBoost(url: string, outFile: string): Promise<void> {
 
         // Resolve on download finished
         req.on('end', () => {
-            console.log("Download finished");
+            console.log('Download finished');
             resolve();
         });
 
         // Fail on error
-        req.on('error', err => {
+        req.on('error', (err) => {
             reject(err);
         });
     });
@@ -155,7 +216,7 @@ export function downloadBoost(url: string, outFile: string): Promise<void> {
 
 /**
  * Delete files
- * 
+ *
  * @param files the files to delete
  */
 export function deleteFiles(files: string[]): void {
@@ -173,22 +234,27 @@ export function deleteFiles(files: string[]): void {
 
 /**
  * Untar boost on linux/macOs
- * 
+ *
  * @param filename the file to extract
  * @param out_dir the output directory
  * @param working_directory the working directory
  */
-function untarLinux(filename: string, out_dir: string, working_directory: string, rename: boolean): Promise<void> {
+function untarLinux(
+    filename: string,
+    out_dir: string,
+    working_directory: string,
+    rename: boolean
+): Promise<void> {
     return new Promise((resolve, reject) => {
-        const args: string[] = ["xzf", filename];
+        const args: string[] = ['xzf', filename];
         if (rename) {
-            args.push("-C", out_dir);
+            args.push('-C', out_dir);
         }
 
         // Use tar to unpack boost
-        const tar = spawn("tar", args, {
+        const tar = spawn('tar', args, {
             stdio: [process.stdin, process.stdout, process.stderr],
-            cwd: working_directory
+            cwd: working_directory,
         });
 
         // Reject/Resolve on close
@@ -196,7 +262,7 @@ function untarLinux(filename: string, out_dir: string, working_directory: string
             if (code != 0) {
                 reject(`Tar exited with code ${code}`);
             } else {
-                console.log("Tar exited with code 0")
+                console.log('Tar exited with code 0');
                 resolve();
             }
         });
@@ -210,16 +276,19 @@ function untarLinux(filename: string, out_dir: string, working_directory: string
 
 /**
  * Unpack boost on windows using 7zip
- * 
+ *
  * @param command the command array to run
  * @param working_directory the working directory to work in
  */
-function run7z(command: Array<string>, working_directory: string): Promise<void> {
+function run7z(
+    command: Array<string>,
+    working_directory: string
+): Promise<void> {
     return new Promise((resolve, reject) => {
         // Spawn a 7z process
-        const tar = spawn("7z", command, {
+        const tar = spawn('7z', command, {
             stdio: [process.stdin, process.stdout, process.stderr],
-            cwd: working_directory
+            cwd: working_directory,
         });
 
         // Reject/Resolve on close
@@ -227,7 +296,7 @@ function run7z(command: Array<string>, working_directory: string): Promise<void>
             if (code != 0) {
                 reject(`7z exited with code ${code}`);
             } else {
-                console.log("7z exited with code 0")
+                console.log('7z exited with code 0');
                 resolve();
             }
         });
@@ -241,22 +310,29 @@ function run7z(command: Array<string>, working_directory: string): Promise<void>
 
 /**
  * Unpack boost using tar on unix or 7zip on windows
- * 
+ *
  * @param base the output base
  * @param working_directory the working directory
  */
-export async function untarBoost(base: string, working_directory: string, rename: boolean = true): Promise<void> {
-    if (process.platform == "win32") {
-        core.debug("Unpacking boost using 7zip");
+export async function untarBoost(
+    base: string,
+    working_directory: string,
+    rename: boolean = true
+): Promise<void> {
+    if (process.platform == 'win32') {
+        core.debug('Unpacking boost using 7zip');
         await run7z(['x', `${base}.tar.gz`], working_directory);
 
         if (rename) {
-            await run7z(['x', `${base}.tar`, '-aoa', `-o${base}`], working_directory);
+            await run7z(
+                ['x', `${base}.tar`, '-aoa', `-o${base}`],
+                working_directory
+            );
         } else {
             await run7z(['x', `${base}.tar`, '-aoa'], working_directory);
         }
     } else {
-        core.debug("Unpacking boost using tar");
+        core.debug('Unpacking boost using tar');
         createDirectory(path.join(working_directory, base));
         await untarLinux(`${base}.tar.gz`, base, working_directory, rename);
     }
@@ -264,13 +340,16 @@ export async function untarBoost(base: string, working_directory: string, rename
 
 /**
  * Clean up
- * 
+ *
  * @param base_dir the base directory
  * @param base the boost base name (without .tar.gz)
  */
 export function cleanup(base_dir: string, base: string) {
-    if (process.platform == "win32") {
-        deleteFiles([path.join(base_dir, `${base}.tar.gz`), path.join(base_dir, `${base}.tar`)]);
+    if (process.platform == 'win32') {
+        deleteFiles([
+            path.join(base_dir, `${base}.tar.gz`),
+            path.join(base_dir, `${base}.tar`),
+        ]);
     } else {
         deleteFiles([path.join(base_dir, `${base}.tar.gz`)]);
     }
