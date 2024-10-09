2e4e7e9043b18fd4b76e0a8df6e157f08cf7e279
daylight <starcrush@monemail.com>
2024-10-09 13:51:41 +0300

    Reformat

--- src/request-progress.d.ts
+++ src/request-progress.d.ts
@@ -6,18 +6,12 @@ declare module 'request-progress' {
 
     interface Progress extends Request {
         on(event: string, listener: (...args: any[]) => void): this;
-        on(
-            event: 'progress',
-            listener: (state: Record<string, any>) => void
-        ): void;
+        on(event: 'progress', listener: (state: Record<string, any>) => void): void;
         on(event: 'request', listener: (req: http.ClientRequest) => void): this;
         on(event: 'response', listener: (resp: Response) => void): this;
         on(event: 'data', listener: (data: Buffer | string) => void): this;
         on(event: 'error', listener: (e: Error) => void): this;
-        on(
-            event: 'complete',
-            listener: (resp: Response, body?: string | Buffer) => void
-        ): this;
+        on(event: 'complete', listener: (resp: Response, body?: string | Buffer) => void): this;
         on(event: 'pipe', listener: (src: stream.Readable) => void): this;
         on(event: 'socket', listener: (src: net.Socket) => void): this;
     }
