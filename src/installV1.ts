725fcfef9efc8f21a73dc9cdd46ac04db9297228
daylight <starcrush@monemail.com>
2024-10-09 13:46:39 +0300

    Reformat

--- src/installV1.ts
+++ src/installV1.ts
@@ -13,37 +13,31 @@ import {
     VersionsRecord,
 } from './shared';
 
-const VERSION_MANIFEST_ADDR: string =
+const VERSION_MANIFEST_ADDR: string = 
     'https://raw.githubusercontent.com/actions/boost-versions/main/versions-manifest.json';
 
 export default async function installV1(opts: Options): Promise<void> {
     console.log('Using legacy install method');
+
     if (opts.cache) {
         console.log('Trying to retrieve cache...');
         if (await restoreCache(opts)) {
             console.log('Cache successfully restored');
             setOutputVariables(
-                path.join(opts.BOOST_ROOT_DIR, 'boost'),
+                path.join(opts.BOOST_ROOT_DIR, 'boost'), 
                 opts.boost_version
             );
             return;
-        } else {
-            console.log('Cache miss');
         }
+        console.log('Cache miss');
     }
 
     console.log('Downloading versions-manifest.json...');
     const versions: VersionsRecord = await getVersions(VERSION_MANIFEST_ADDR);
-
     const { boost_version, toolset, platform_version, BOOST_ROOT_DIR } = opts;
 
     console.log('Parsing versions-manifest.json...');
-    const ver_data = parseArguments(
-        versions,
-        boost_version,
-        toolset,
-        platform_version
-    );
+    const ver_data = parseArguments(versions, boost_version, toolset, platform_version);
     const download_url = ver_data.url;
     const filename = ver_data.filename;
 
