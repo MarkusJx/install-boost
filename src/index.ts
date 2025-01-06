1e6c8330d4e70122662a92ee48a0079d44390241
Markus <28785953+MarkusJx@users.noreply.github.com>
2025-01-06 12:55:23 +0100

    ci: add lint workflow (#48)

--- src/index.ts
+++ src/index.ts
@@ -9,116 +9,116 @@ import * as semver from 'semver';
 let BOOST_ROOT_DIR: string = path.join(process.env.GITHUB_WORKSPACE!, 'boost');
 
 async function main(): Promise<void> {
-    const boost_version: string = core.getInput('boost_version');
-    const toolset: string = core.getInput('toolset');
-    const platform_version: string = core.getInput('platform_version');
-    const boost_install_dir: string = core.getInput('boost_install_dir');
-    const link: string = core.getInput('link');
-    const arch: string = core.getInput('arch');
-    const cache: boolean = core.getBooleanInput('cache') ?? true;
-    let script_version: string = core.getInput('version');
+  const boost_version: string = core.getInput('boost_version');
+  const toolset: string = core.getInput('toolset');
+  const platform_version: string = core.getInput('platform_version');
+  const boost_install_dir: string = core.getInput('boost_install_dir');
+  const link: string = core.getInput('link');
+  const arch: string = core.getInput('arch');
+  const cache: boolean = core.getBooleanInput('cache') ?? true;
+  let script_version: string = core.getInput('version');
 
-    if (boost_version.length <= 0) {
-        throw new Error('the boost_version variable must be defined');
+  if (boost_version.length <= 0) {
+    throw new Error('the boost_version variable must be defined');
+  }
+
+  if (cache) {
+    console.log('Using @action/cache to improve build times');
+  }
+
+  if (boost_install_dir.length > 0) {
+    BOOST_ROOT_DIR = path.join(boost_install_dir, 'boost');
+    console.log(
+      `The install directory was manually changed to ${BOOST_ROOT_DIR}`
+    );
+  }
+
+  if (!script_version) {
+    script_version = 'default';
+  }
+
+  if (!platform_version) {
+    core.warning(
+      "The 'platform_version' input is unset. This may lead to inconsistent build results."
+    );
+  }
+
+  const semverReplace = (version: string) =>
+    version.replace(/([.\d]+)\.(beta|alpha)(\d+)/gi, '$1-$2.$3');
+
+  if (
+    !toolset &&
+    process.platform === 'win32' &&
+    (semver.gte(semverReplace(boost_version), '1.78.0') ||
+      script_version === 'legacy')
+  ) {
+    core.warning(
+      "The 'toolset' input is unset. This may lead to inconsistent build results."
+    );
+  } else if (
+    toolset &&
+    semver.lt(semverReplace(boost_version), '1.78.0') &&
+    script_version !== 'legacy'
+  ) {
+    core.warning(
+      'Setting the toolset with boost version < 1.78.0 may cause issues'
+    );
+  }
+
+  if (
+    link &&
+    link !== 'static' &&
+    link !== 'shared' &&
+    link !== 'static+shared'
+  ) {
+    throw new Error(
+      "'link' must be one of: 'static', 'shared' or 'static+shared'"
+    );
+  }
+
+  if (arch && arch !== 'x86' && arch !== 'aarch64') {
+    throw new Error("'arch' must be one of: 'x86' or 'aarch64'");
+  }
+
+  if (script_version === 'legacy') {
+    if (link) {
+      core.warning(
+        "The script version was set to 'legacy', but the 'link' option was supplied, ignoring this"
+      );
     }
 
-    if (cache) {
-        console.log('Using @action/cache to improve build times');
-    }
-
-    if (boost_install_dir.length > 0) {
-        BOOST_ROOT_DIR = path.join(boost_install_dir, 'boost');
-        console.log(
-            `The install directory was manually changed to ${BOOST_ROOT_DIR}`
-        );
-    }
-
-    if (!script_version) {
-        script_version = 'default';
-    }
-
-    if (!platform_version) {
-        core.warning(
-            "The 'platform_version' input is unset. This may lead to inconsistent build results."
-        );
-    }
-
-    const semverReplace = (version: string) =>
-        version.replace(/([.\d]+)\.(beta|alpha)(\d+)/gi, '$1-$2.$3');
-
-    if (
-        !toolset &&
-        process.platform === 'win32' &&
-        (semver.gte(semverReplace(boost_version), '1.78.0') ||
-            script_version === 'legacy')
-    ) {
-        core.warning(
-            "The 'toolset' input is unset. This may lead to inconsistent build results."
-        );
-    } else if (
-        toolset &&
-        semver.lt(semverReplace(boost_version), '1.78.0') &&
-        script_version !== 'legacy'
-    ) {
-        core.warning(
-            'Setting the toolset with boost version < 1.78.0 may cause issues'
-        );
-    }
-
-    if (
-        link &&
-        link !== 'static' &&
-        link !== 'shared' &&
-        link !== 'static+shared'
-    ) {
-        throw new Error(
-            "'link' must be one of: 'static', 'shared' or 'static+shared'"
-        );
-    }
-
-    if (arch && arch !== 'x86' && arch !== 'aarch64') {
-        throw new Error("'arch' must be one of: 'x86' or 'aarch64'");
-    }
-
-    if (script_version === 'legacy') {
-        if (link) {
-            core.warning(
-                "The script version was set to 'legacy', but the 'link' option was supplied, ignoring this"
-            );
-        }
-
-        await installV1({
-            boost_version,
-            toolset,
-            platform_version,
-            BOOST_ROOT_DIR,
-            cache,
-        });
-    } else if (script_version === 'default') {
-        await installV2({
-            boost_version,
-            toolset,
-            platform_version,
-            link,
-            arch,
-            BOOST_ROOT_DIR,
-            cache,
-        });
-    } else {
-        throw new Error("Invalid value entered for option 'version'");
-    }
+    await installV1({
+      boost_version,
+      toolset,
+      platform_version,
+      BOOST_ROOT_DIR,
+      cache,
+    });
+  } else if (script_version === 'default') {
+    await installV2({
+      boost_version,
+      toolset,
+      platform_version,
+      link,
+      arch,
+      BOOST_ROOT_DIR,
+      cache,
+    });
+  } else {
+    throw new Error("Invalid value entered for option 'version'");
+  }
 }
 
 try {
-    console.log(`Starting install-boost@${version}`);
-    main().then(
-        () => {
-            console.log(`install-boost@${version} finished successfully`);
-        },
-        (reject) => {
-            core.setFailed(reject);
-        }
-    );
+  console.log(`Starting install-boost@${version}`);
+  main().then(
+    () => {
+      console.log(`install-boost@${version} finished successfully`);
+    },
+    (reject) => {
+      core.setFailed(reject);
+    }
+  );
 } catch (error: any) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
--- src/installV2.ts
+++ src/installV2.ts
@@ -2,82 +2,82 @@ import * as core from '@actions/core';
 import * as path from 'path';
 import { restoreCache, saveCache } from './cache';
 import {
-    cleanup,
-    createDirectory,
-    downloadBoost,
-    getVersions,
-    OptionsV2,
-    parseArguments,
-    setOutputVariables,
-    untarBoost,
-    VersionsRecord,
+  cleanup,
+  createDirectory,
+  downloadBoost,
+  getVersions,
+  OptionsV2,
+  parseArguments,
+  setOutputVariables,
+  untarBoost,
+  VersionsRecord,
 } from './shared';
 
 const VERSION_MANIFEST_ADDR: string =
-    'https://raw.githubusercontent.com/MarkusJx/prebuilt-boost/main/versions-manifest.json';
+  'https://raw.githubusercontent.com/MarkusJx/prebuilt-boost/main/versions-manifest.json';
 
 export default async function installV2(opts: OptionsV2): Promise<void> {
-    if (opts.cache) {
-        console.log('Trying to retrieve cache...');
-        if (await restoreCache(opts)) {
-            console.log('Cache successfully restored');
-            setOutputVariables(
-                path.join(opts.BOOST_ROOT_DIR, 'boost'),
-                opts.boost_version
-            );
-            return;
-        } else {
-            console.log('Cache miss');
-        }
+  if (opts.cache) {
+    console.log('Trying to retrieve cache...');
+    if (await restoreCache(opts)) {
+      console.log('Cache successfully restored');
+      setOutputVariables(
+        path.join(opts.BOOST_ROOT_DIR, 'boost'),
+        opts.boost_version
+      );
+      return;
+    } else {
+      console.log('Cache miss');
     }
+  }
 
-    console.log('Downloading versions-manifest.json...');
-    const versions: VersionsRecord = await getVersions(VERSION_MANIFEST_ADDR);
+  console.log('Downloading versions-manifest.json...');
+  const versions: VersionsRecord = await getVersions(VERSION_MANIFEST_ADDR);
 
-    const {
-        boost_version,
-        toolset,
-        platform_version,
-        link,
-        arch,
-        BOOST_ROOT_DIR,
-    } = opts;
+  const {
+    boost_version,
+    toolset,
+    platform_version,
+    link,
+    arch,
+    BOOST_ROOT_DIR,
+  } = opts;
 
-    console.log('Parsing versions-manifest.json...');
-    const ver_data = parseArguments(
-        versions,
-        boost_version,
-        toolset,
-        platform_version,
-        link,
-        arch
-    );
-    const download_url: string = ver_data.url;
-    const filename: string = ver_data.filename;
+  console.log('Parsing versions-manifest.json...');
+  const ver_data = parseArguments(
+    versions,
+    boost_version,
+    toolset,
+    platform_version,
+    link,
+    arch
+  );
+  const download_url: string = ver_data.url;
+  const filename: string = ver_data.filename;
 
-    core.startGroup(`Create ${BOOST_ROOT_DIR}`);
-    createDirectory(BOOST_ROOT_DIR);
-    core.endGroup();
+  core.startGroup(`Create ${BOOST_ROOT_DIR}`);
+  createDirectory(BOOST_ROOT_DIR);
+  core.endGroup();
 
-    core.startGroup('Download Boost');
-    await downloadBoost(download_url, path.join(BOOST_ROOT_DIR, filename));
-    core.endGroup();
+  core.startGroup('Download Boost');
+  await downloadBoost(download_url, path.join(BOOST_ROOT_DIR, filename));
+  core.endGroup();
 
-    let base_dir: string = filename.substring(0, filename.lastIndexOf('.'));
-    base_dir = base_dir.substring(0, base_dir.lastIndexOf('.'));
+  let base_dir: string = filename.substring(0, filename.lastIndexOf('.'));
+  base_dir = base_dir.substring(0, base_dir.lastIndexOf('.'));
 
-    core.startGroup(`Extract ${filename}`);
-    await untarBoost(base_dir, BOOST_ROOT_DIR, false);
-    core.endGroup();
+  core.startGroup(`Extract ${filename}`);
+  await untarBoost(base_dir, BOOST_ROOT_DIR, false);
+  core.endGroup();
 
-    core.startGroup('Clean up');
-    cleanup(BOOST_ROOT_DIR, base_dir);
-    core.endGroup();
+  core.startGroup('Clean up');
+  cleanup(BOOST_ROOT_DIR, base_dir);
+  core.endGroup();
 
-    setOutputVariables(path.join(BOOST_ROOT_DIR, 'boost'), base_dir);
+  setOutputVariables(path.join(BOOST_ROOT_DIR, 'boost'), base_dir);
 
-    if (opts.cache) {
-        console.log('Saving cache');
-        saveCache(opts);
-    }
+  if (opts.cache) {
+    console.log('Saving cache');
+    saveCache(opts);
+  }
 }
